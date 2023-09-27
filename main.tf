46785a39325ba9d2588f64165951f458a71dd0f6
Michael Graff <meg@dolby.com>
2023-09-27 16:18:52 +0000

    25 refactor s3 bucket into new terrahouse_aws module

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
-resource "aws_s3_bucket" "website_bucket" {
-   bucket = var.bucket_name
+terraform {
+  # cloud {
+  #   organization = "cloudyadvice"
 
-   tags = {
-     UserUuid    = "var.user_uuid"
-     Environment = "Dev"
-   }
-} 
+  # workspaces {
+  #   name = "terra-house-1"
+  # }
+}
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
