9667cc0caa621df669439922f86c6cf3884d966e
Michael Graff <38571586+michaelgraff@users.noreply.github.com>
2023-09-27 10:33:15 -0500

    #23 bucket no longer uses random provider (#24)

--- variables.tf
+++ variables.tf
@@ -7,3 +7,12 @@ variable "user_uuid" {
     error_message = "Invalid user UUID format. It should be in the format of 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', where x is a hexadecimal digit."
   }
 }
+variable "bucket_name" {
+  description = "The name of the AWS S3 bucket"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9.-]{3,63}$", var.bucket_name))
+    error_message = "The bucket name must be between 3 and 63 characters long and can only contain alphanumeric characters, hyphens, and periods."
+  }
+}
