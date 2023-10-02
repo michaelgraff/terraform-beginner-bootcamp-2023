98270053452702409d11e02dd328dc5c48838e98
Michael Graff <meg@dolby.com>
2023-10-02 19:04:40 +0000

    edit tags

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -3,8 +3,8 @@ resource "aws_s3_bucket" "website_bucket" {
 
    tags = {
      UserUuid    = "var.user_uuid"
-     Environment = "Dev"
-     Hello       = "saturn"
+     Environment = "Development"
+     Hello       = "jupiter"
    }
 } 
 
