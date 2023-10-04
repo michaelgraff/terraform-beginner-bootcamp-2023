64727d071ffd948fe99f600e2856956f4bbdd92d
Michael Graff <meg@dolby.com>
2023-10-04 23:38:53 +0000

    #45 finish implementation of terraform provider

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,6 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:DTFFt/I7gOQEI26eJ1GJyNvCuup9G6b3V2FUvETdW0A=",
+    "h1:IYh+2Uf+ukXMlB/il8yPHkqCm59PEx/ImjJYsx2jtuU=",
   ]
 }
--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -28,3 +28,17 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
+
+resource "terratowns_home" "home" {
+  name = "Easy Stars All Stars"
+  description = <<DESCRIPTION
+Easy Star All-Stars is a reggae collective founded in 1997 by Michael Goldwasser,
+Eric Smith, Lem Oppenheimer and Remy Gerstein of New York City-based Easy Star Records.
+The band is known for its reinterpretations of classic albums in reggae style.
+Their first album, released in 2003, was Dub Side of the Moon, an interpretation of Pink Floyd's 1973 album Dark Side of the Moon.
+  DESCRIPTION
+  # domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "abcd3fg.cloudfront.net"
+  town = "melomaniac-mansion"
+  content_version = 1
+}
